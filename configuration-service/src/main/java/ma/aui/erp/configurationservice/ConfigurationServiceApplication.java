6a2d1b637b1e1439ab8ff7d95987116572299c93
Soufiane KARROUMI <soufianekarroumi@outlook.com>
2021-10-10 21:09:46 +0100

    Initial Configuration and Mapping of Supporting Services

--- configuration-service/src/main/java/ma/aui/erp/configurationservice/ConfigurationServiceApplication.java
+++ configuration-service/src/main/java/ma/aui/erp/configurationservice/ConfigurationServiceApplication.java
@@ -2,7 +2,9 @@ package ma.aui.erp.configurationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigurationServiceApplication {
 
--- discovery-service/src/main/java/ma/aui/erp/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/ma/aui/erp/discoveryservice/DiscoveryServiceApplication.java
@@ -1,8 +1,10 @@
 package ma.aui.erp.discoveryservice;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServiceApplication {
 
